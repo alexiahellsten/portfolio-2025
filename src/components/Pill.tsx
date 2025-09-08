573ac49760519ad015b1631a13698c2329db3e4c
Alexia Hellsten <alexia.hellsten@gmail.com>
2025-09-08 17:39:14 +0200

    Change text size for Pill component

--- src/components/Pill.tsx
+++ src/components/Pill.tsx
@@ -1,7 +1,7 @@
 function Pill({ children }: { children: React.ReactNode }) {
 
   return (
-    <span className="markdown inline-block bg-[#172025] text-white px-3 py-1 rounded-full text-xs font-medium mr-2 mb-2">
+    <span className="markdown inline-block bg-[#172025] text-white px-3 py-1 rounded-full text-sm font-medium mr-2 mb-2">
       {children}
     </span>
   )
