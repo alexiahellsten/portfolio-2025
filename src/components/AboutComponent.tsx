1eea94aef57c7bf82dad8e73c119d83ba2639ff0
Alexia Hellsten <alexia.hellsten@gmail.com>
2025-09-08 17:39:28 +0200

    Edit About text

--- src/components/AboutComponent.tsx
+++ src/components/AboutComponent.tsx
@@ -7,11 +7,12 @@ export default function About() {
       <h3 className="text-3xl md:text-4xl font-bold mb-8 text-center">
         About me
       </h3>
-  <p className="max-w-2xl mx-auto text-base md:text-lg text-left leading-relaxed">
-        I'm a frontend development student at EC Utbildning. I'm passionate about working across the whole stack and care deeply about the human experience behind the tech.<br /><br />
-        I fell in love with programming is because it's fun and creative, there is always something new to learn!<br /><br />
-        I'm very eager to gain hands-on experience, which is why I'm currently looking for a LIA-internship in Stockholm from November 2025 to May 2026.<br /><br />
-      </p>
+      <p className="max-w-2xl mx-auto text-base md:text-lg text-left leading-relaxed">
+            I'm a 29 year old frontend development student at EC Utbildning. I'm passionate about working across the whole stack and care deeply about the human experience behind the tech.<br /><br />
+            I fell in love with programming is because it's fun and creative, there is always something new to learn!<br /><br />
+            When I’m not coding, I'm most likely sketching out an art project, curating the perfect playlist, projecting the climbing wall or sipping coffee like it’s part of my tech stack.<br /><br />
+            I'm very eager to learn more and gain hands-on experience, which is why I'm currently looking for a LIA-internship in Stockholm from November 2025 to May 2026.<br /><br />
+          </p>
     </section>
   )
 }
