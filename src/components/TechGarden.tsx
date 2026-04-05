ce9e3580c8e223ad09e405dc9958ccb5c8d5e5c7
Alexia Hellsten <alexia.hellsten@gmail.com>
2026-04-05 14:03:17 +0200

    Update techgarden section and translations

--- src/components/TechGarden.tsx
+++ src/components/TechGarden.tsx
@@ -21,32 +21,44 @@ export default function TechGarden() {
       <div className='text-base md:text-lg text-left leading-relaxed'>
         <h3 className='font-semibold mt-8'>{techgarden.subheading1}</h3>
         <p className='py-4'>
-          🌾 <Pill>JavaScript</Pill> <Pill>TypeScript</Pill> <Pill>HTML</Pill>{" "}
-          <Pill>CSS</Pill> <Pill>Sass</Pill> <Pill>Tailwind</Pill>{" "}
-          <Pill>Bulma</Pill>
+          <Pill>JavaScript</Pill> <Pill>TypeScript</Pill> <Pill>C#</Pill>{" "}
+          <Pill>HTML</Pill> <Pill>CSS</Pill> <Pill>Sass</Pill>{" "}
+          <Pill>Tailwind CSS</Pill>{" "}
         </p>
 
         <h3 className='font-semibold mt-4'>{techgarden.subheading2}</h3>
         <p className='py-4'>
-          🌻 <Pill>React</Pill> <Pill>Angular</Pill>
+          <Pill>React</Pill> <Pill>Next.js (React framework)</Pill>{" "}
+          <Pill>Angular</Pill>
         </p>
 
         <h3 className='font-semibold mt-4'>{techgarden.subheading3}</h3>
         <p className='py-4'>
-          🌲 <Pill>Node.js</Pill> <Pill>Express</Pill> <Pill>MongoDB</Pill>{" "}
-          <Pill>Mongoose</Pill> <Pill>SQLite</Pill>
+          <Pill>Node.js (Express)</Pill> <Pill>ASP.NET Core</Pill>{" "}
+          <Pill>Express</Pill> <Pill>MongoDB (Mongoose)</Pill>{" "}
+          <Pill>SQLite</Pill>
         </p>
 
         <h3 className='font-semibold mt-4'>{techgarden.subheading4}</h3>
         <p className='py-4'>
-          🐞 <Pill>Git</Pill> <Pill>Cypress</Pill> <Pill>Postman</Pill>{" "}
-          <Pill>Figma</Pill>
+          <Pill>Git</Pill> <Pill>GitHub</Pill> <Pill>Azure DevOps</Pill>{" "}
+          <Pill>Postman</Pill>
         </p>
 
         <h3 className='font-semibold mt-4'>{techgarden.subheading5}</h3>
         <p className='py-4'>
-          🍃 <Pill>11ty</Pill> <Pill>Handlebars</Pill> <Pill>Nunjucks</Pill>{" "}
-          <Pill>EJS</Pill>
+          <Pill>Cypress</Pill>
+        </p>
+
+        <h3 className='font-semibold mt-4'>{techgarden.subheading6}</h3>
+        <p className='py-4'>
+          <Pill>Figma</Pill>
+        </p>
+
+        <h3 className='font-semibold mt-4'>{techgarden.subheading7}</h3>
+        <p className='py-4'>
+          <Pill>Eleventy (11ty)</Pill> <Pill>Handlebars</Pill>{" "}
+          <Pill>Nunjucks</Pill> <Pill>EJS</Pill>
         </p>
       </div>
     </section>
